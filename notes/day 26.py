9d840d4d62bc6abd84a34e2e488f39d302915e71
kundanmanagre04 <kundanmanagre04@gmail.com>
2025-11-10 14:16:40 +0530

    git

--- notes/day 26.py
+++ notes/day 26.py
@@ -1,7 +1,7 @@
 '''git=this is a version control system it use to create check point to your code 
 github=it is a cloud based service that store my code'''
 '''commands '''
-# 1) git init (connet this folder to git ) if you are not in current directory and cd directory name)
+# 1) git init (connect this folder to git ) if you are not in current directory and cd directory name)
 # 2) git add . (all change will add )
 # 3) git commit -m "message here "
 # 4) git push origin main ("all code upload to github ")
@@ -9,4 +9,11 @@ github=it is a cloud based service that store my code'''
 # 6) git chekout ("its chenge the brance ")
 # 7) git branch kundan ("new branch created name kundan")
 # 8) git branch -d branch_name (" delete the branch ")
-# 9) cd /d c:f1/f2.txt (this is to back to c)
+# 9) git status ("its show status of all file and what new change you do in files")
+# 10) git rm --file name ("its remove file  ")
+# 11) git log --oneline ("how many opration performm in branch ")
+# 13) git log ("its show username and email of login people ")
+# 14) git config --global user.name "<your_username>" ("this is to add username to git ")
+# 15) git config --global user.email "<your_email>"  ("this is to add email to git")
+# 16) git remote <link of repo> ("this is use to connect to push local code to github")
+# 17) 
