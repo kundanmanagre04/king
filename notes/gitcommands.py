f8ee810cc32ee0fcfa8a92e0b4eb298b0a758e95
kundanmanagre04 <kundanmanagre04@gmail.com>
2025-11-11 10:37:15 +0530

    git file

---
+++ notes/gitcommands.py
@@ -0,0 +1,23 @@
+'''git=this is a version control system it use to create check point to your code 
+github=it is a cloud based service that store my code'''
+'''commands '''
+# 1) git init (connect this folder to git ) if you are not in current directory and cd directory name)
+# 2) git add . (all change will add )
+# 3) git commit -m "message here " ("bind all data to push ")
+# 4) git push origin main ("all code upload to github ")
+# 5) git branch ("its provide current working brance ")
+# 6) git chekout branchname  ("its chenge the brance ")
+# 7) git branch kundan ("new branch created name kundan")
+# 8) git branch -d branch_name (" delete the branch ")
+# 9) git status ("its show status of all file and what new change you do in files")
+# 10) git rm --file name ("its remove file  ")
+# 11) git log --oneline ("how many opration performm in branch ")
+# 13) git log ("its show username and email of login people ")
+# 14) git config --global user.name "<your_username>" ("this is to add username to git ")
+# 15) git config --global user.email "<your_email>"  ("this is to add email to git")
+# 16) git remote <link of repo> ("this is use to connect to push local code to github")
+# 17) git ignore me jo file hogi wo ignore ho jayegi git se 
+# 18) .gitkeep (" jo empty folder ko fetch karta hai github ")
+# 19) git.switch nameofbranch ("change karega branch ko lekin sara data upload ho na chaye")
+# 20) git stash ("this is to sove change in temporary local area and then you can change a branch ")
+# 21) 
